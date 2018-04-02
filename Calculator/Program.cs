f6d60effeb1674e4f086a31d4fceca26f40adb39
Tanguy Herrmann <dolanorgit@evereska.org>
2018-04-02 16:49:47 +0200

    Add operations

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -8,9 +8,38 @@ namespace Calculator
 {
     class Program
     {
+        private static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        private static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        
+        private static int Mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        private static int Floor(int a, int b)
+        {
+            return a / b;
+        }
+
+        private static float Div(float a, int b)
+        {
+            return a / b;
+        }
+
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World.");
+            Console.WriteLine("Hello World. {0} {1} {2} {3}", Add(1, 2), Sub(1, 2), Mul(2, 4), Div(3, 2));
+            Console.ReadKey();
         }
     }
 }
